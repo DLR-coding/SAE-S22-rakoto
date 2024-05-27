b58f7ef106ca2eaa97fe056f9a5b9f6b4d13ad44
Dylan Rakotomahefa <dylan@unistra.fr>
2024-05-27 13:48:49 +0200

    Premier commit

---
+++ m23/main.c
@@ -0,0 +1,23 @@
+#include "graphe.h"
+#include "algos.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+
+    graphe g;
+    init_graphe(&g);
+
+    printf("AFFICHER GRAPHE\n");
+    printf("===============\n");
+    afficher(&g);
+
+    free_graphe(&g);
+
+    return EXIT_SUCCESS;
+}
